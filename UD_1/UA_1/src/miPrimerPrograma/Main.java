1ffe41446d28fe6139bf0f0203fcc91c53999ede
pistolo <pistolo@pistolo.com>
2022-07-28 19:40:57 +0200

    API java

--- UD_1/UA_1/src/miPrimerPrograma/Main.java
+++ UD_1/UA_1/src/miPrimerPrograma/Main.java
@@ -1,8 +1,12 @@
+package miPrimerPrograma;
 
 public class Main {
 
 	public static void main(String[] args) {
+
 		System.out.println("Hola Mundo");
+		System.out.print("Adios Mundo");
+		System.out.print("Otra cosa Mundo");
 	}
 
 }
