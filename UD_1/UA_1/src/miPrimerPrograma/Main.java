83d6fab84830b2f3cf33727ccb7ee4a5e4225776
pistolo <pistolo@pistolo.com>
2022-07-28 20:11:52 +0200

    EJERCICIO SECUENCIAS DE ESCAPE

--- UD_1/UA_1/src/miPrimerPrograma/Main.java
+++ UD_1/UA_1/src/miPrimerPrograma/Main.java
@@ -7,6 +7,14 @@ public class Main {
 		System.out.println("Hola Mundo");
 		System.out.print("Adios Mundo");
 		System.out.print("Otra cosa Mundo");
+		// retorno de carro
+		System.out.print("\r");
+		System.out.print("Cadena de texto con secuencia de salto de línea\n");
+		System.out.print("Segunda línea con secuencia tabulador\tQue separa unos espacios el texto\n"); // Ojo bug con /b
+		System.out.print("Tercera línea borrado carácter izquierda\b y tabulador\t de ejemplo\n");
+		System.out.print("Cuarta línea mostramos una contrabarra \\ \n");
+		System.out.print("Quinta línea mostramos una comilla doble \" \n");
+		System.out.print("Sexta línea mostramos una comilla simple \' \n");
 	}
 
 }
