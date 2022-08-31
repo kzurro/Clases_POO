843c8365e3a9a0e3f477322c106f1fab9ef63365
kzurro <kzurro@hotmail.com>
2022-08-31 11:35:34 +0200

    Ejemplo Operador ternario UD1 UA4

---
+++ UD_1/UA_1/src/operandos/Ternario.java
@@ -0,0 +1,18 @@
+package operandos;
+
+public class Ternario {
+
+	public static void main(String[] args) {
+		int a, b;
+
+		a = 3 < 5 ? 1 : -1;
+
+		System.out.println(a);
+
+		b = a == 7 ? 10 : 20;
+
+		System.out.println(b);
+
+	}
+
+}
