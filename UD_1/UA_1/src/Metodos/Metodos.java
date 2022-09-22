aa9cc3aac80486a8e1ea57ced0e0067390f5dfb3
kzurro <pistolo@pistolo.com>
2022-09-22 10:39:00 +0200

    Ejemplo de parámetros en métodos

--- UD_1/UA_1/src/Metodos/Metodos.java
+++ UD_1/UA_1/src/Metodos/Metodos.java
@@ -4,19 +4,25 @@ public class Metodos {
 
 	public static void main(String[] args) {
 
-		tresSaludos();
-		tresSaludos();
+		nuevaLinea("Hola, ¡Buenos Días!", 5);
+		nuevaLinea("|----------------- otra linea -----------------|");
+		nuevaLinea(5);
+		nuevaLinea("|----------------- otra linea -----------------|", 2);
 
 	}
 
-	public static void tresSaludos() {
-		unSaludo();
-		unSaludo();
-		unSaludo();
+	public static void nuevaLinea(int num) {
+		nuevaLinea("", num);
 	}
 
-	public static void unSaludo() {
-		System.out.println("Hola, !buenos días!");
+	public static void nuevaLinea(String mensaje, int num) {
+		for (int i = 0; i < num; i++) {
+			System.out.println(mensaje);
+		}
+	}
+
+	public static void nuevaLinea(String mensaje) {
+		nuevaLinea(mensaje, 1);
 	}
 
 }
