95ea0747094841831f00ad731178cb195f68a326
kzurro <pistolo@pistolo.com>
2022-09-22 11:28:57 +0200

    Ejemplo de Rersividad

---
+++ UD_1/UA_1/src/Metodos/Recursividad.java
@@ -0,0 +1,22 @@
+package Metodos;
+
+public class Recursividad {
+
+	public static void main(String[] args) {
+
+		System.out.println(factorial(5));
+
+	}
+
+	static int factorial(int numero) {
+		int resultado;
+
+		if (numero != 1 && numero > 0) {
+			resultado = factorial(numero - 1) * numero;
+		} else {
+			resultado = 1;
+		}
+		return resultado;
+	}
+
+}
