fdf1770a0c81398970cc3c8637977a8d27afe0ea
kzurro <pistolo@pistolo.com>
2022-09-22 09:26:18 +0200

    Ejemplo método

---
+++ UD_1/UA_1/src/Metodos/Metodos.java
@@ -0,0 +1,31 @@
+package Metodos;
+
+public class Metodos {
+
+	public static void main(String[] args) {
+
+		//tresSaludos();
+		
+		
+		String mensaje = "Hola";
+		
+		String otroMensaje = mensaje;
+		
+		System.out.println(mensaje);
+		System.out.println(otroMensaje);
+		
+		mensaje  ="adios";
+		System.out.println(mensaje);
+		
+		System.out.println(otroMensaje);
+		System.out.println(otroMensaje);
+
+	}
+	
+	public static void tresSaludos() {
+		System.out.println("Hola, !buenos días!");
+		System.out.println("Hola, !buenos días!");
+		System.out.println("Hola, !buenos días!");
+		}
+
+}
