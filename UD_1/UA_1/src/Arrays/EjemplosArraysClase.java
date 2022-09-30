8fb4b64013b952210999b0df6b7613c10309c997
kzurro <pistolo@pistolo.com>
2022-09-30 12:45:44 +0200

    Ejemplo patriz dos dimendiones

--- UD_1/UA_1/src/Arrays/EjemplosArraysClase.java
+++ UD_1/UA_1/src/Arrays/EjemplosArraysClase.java
@@ -91,6 +91,34 @@ public class EjemplosArraysClase {
 
 			}
 
+			/*
+			 * Matrices de dos dimensiones
+			 */
+
+			int[][] matriz = { { 1, 2, 3, 4, 5, 6 }, { 9, 8, 7, 6, 8, 9 }, { 9, 8, 7, 6, 8, 9 }, { 9, 8, 7, 6, 8, 9 },
+					{ 9, 8, 7, 6, 8, 9 } };
+			System.out.println(matriz.length);
+			System.out.println(matriz);
+			System.out.println(Arrays.toString(matriz));
+
+			for (int k = 0; k < matriz.length; k++) {
+				for (int j = 0; j < matriz[0].length; j++) {
+					// hacer la operación que se necesite
+					System.out.print(matriz[k][j] + "\t");
+					// uso \t para alinear las columnas
+				}
+				System.out.println(); // siguiente fila
+
+			}
+
+			System.out.println("----------------------------------------------------------------------------");
+
+			for (int[] numero : matriz) {
+				for (int num : numero) {
+					System.out.print(num + "\t");
+				}
+				System.out.println();
+			}
 		}
 
 	}
