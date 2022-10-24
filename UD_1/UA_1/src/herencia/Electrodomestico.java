58fc51a89d403854d52e1be48b8bd426eab5e4b6
kzurro <pistolo@pistolo.com>
2022-10-24 10:31:54 +0200

    UD2-UA5: Ejercicio Electrodomestico 2ª parte

--- UD_1/UA_1/src/herencia/Electrodomestico.java
+++ UD_1/UA_1/src/herencia/Electrodomestico.java
@@ -1,11 +1,13 @@
 package herencia;
 
 public class Electrodomestico {
+
 	// Constantes
 
 	private final static String COLOR_DEF = "blanco";
 	private final static char CONSUMO_ENERGETICO_DEF = 'F';
 	private final static double PRECIO_BASE_DEF = 100;
+	private final static String[] COLORES = { "blanco", "negro", "rojo", "azul", "gris" };
 
 	/**
 	 * Peso por defecto
@@ -35,6 +37,10 @@ public class Electrodomestico {
 
 	public static double getPesoDef() {
 		return PESO_DEF;
+	}
+
+	public static String[] getColores() {
+		return COLORES;
 	}
 
 	public double getPrecioBase() {
@@ -82,8 +88,82 @@ public class Electrodomestico {
 	public Electrodomestico(double precioBase, double peso, char consumoEnergetico, String color) {
 		setPrecioBase(precioBase);
 		setPeso(peso);
-		setConsumoEnergetico(consumoEnergetico);
-		setColor(color);
+		comprobarConsumoEnergetico(consumoEnergetico);
+		comprobarColor(color);
+	}
+
+	/**
+	 * 
+	 * @param consumoEnergetico
+	 * @see <a href=
+	 *      "https://es.m.wikipedia.org/wiki/Archivo:ASCII-Table-wide.svg"/>Equivalencia
+	 *      Char ASCII</a>
+	 * 
+	 *      Solo tenido en cuenta las mayúsculas
+	 */
+	private void comprobarConsumoEnergetico(char consumoEnergetico) {
+
+		if (consumoEnergetico >= 65 && consumoEnergetico <= 70) {
+			setConsumoEnergetico(consumoEnergetico);
+		} else {
+			setConsumoEnergetico(getConsumoEnergeticoDef());
+		}
+
+	}
+
+	private void comprobarColor(String color) {
+
+		boolean encontrado = false;
+
+		for (int i = 0; i < getColores().length && !encontrado; i++) {
+			if (getColores()[i].equals(color)) {
+				encontrado = true;
+			}
+
+		}
+
+		if (encontrado) {
+			setColor(color);
+		} else {
+			setColor(COLOR_DEF);
+		}
+
+	}
+
+	public double precioFinal() {
+		double plus = 0;
+		switch (consumoEnergetico) {
+		case 'A':
+			plus += 100;
+			break;
+		case 'B':
+			plus += 80;
+			break;
+		case 'C':
+			plus += 60;
+			break;
+		case 'D':
+			plus += 50;
+			break;
+		case 'E':
+			plus += 30;
+			break;
+		case 'F':
+			plus += 10;
+			break;
+		}
+
+		if (peso >= 0 && peso < 19) {
+			plus += 10;
+		} else if (peso >= 20 && peso < 49) {
+			plus += 50;
+		} else if (peso >= 50 && peso <= 79) {
+			plus += 80;
+		} else if (peso >= 80) {
+			plus += 100;
+		}
+
+		return precioBase + plus;
 	}
 
 }
